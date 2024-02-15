098172ff51960971b4b00e0777a511980f7956c9
Joanna Hosking <hoskingjoanna@gmail.com>
2024-02-15 19:59:50 +0000

    more accessibility labeling fixes

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,7 +6,7 @@ const Slider = (props) => {
         <div className="shift-slider">
         <label for="shift">Shift:</label>
         <div className="wrapper">
-            <input name="shift" type="range" min="0" max={max} value={shift} onChange={(e) => setShift(e.target.value)} />
+            <input name="shift" id="shift" type="range" min="0" max={max} value={shift} onChange={(e) => setShift(e.target.value)} />
             <span className="slide" style={{transform: `scaleX(${shift / max})`}}></span>
             <span className="background"></span>
             <span className="handle" style={{left: (shift / max) * 100 + '%'}}>{shift}</span>
