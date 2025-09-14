61d5a235a9b10d1980c55902663466d2d105071a
Joanna Hosking <hoskingjoanna@gmail.com>
2025-09-14 13:38:15 +0100

    transitioned to nested css, added flavor text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Slider from './components/Slider';
 import Primary from "./palettes/Primary";
 import Complementary from "./palettes/Complementary";
 import Triadic from "./palettes/Triadic";
-import "./sass/App.scss";
+import "./css/App.css";
 
 function App() {
   const [primary, setPrimary] = useState("#e26969");
@@ -13,6 +13,7 @@ function App() {
     <div className="App">
       <h1>Palette Generator</h1>
       <p>Generate a series of hue shifted palettes. Hue shifting combines hue alongside lightness when shading and highlighting. Select the central colour of your primary palette. Adjust the shift higher to intensify the effect or adjust lower to generate a monochromatic palette.</p>
+      <p>If you found this tool helpful, I'd love to see what you created. Drop me an email at <a href="mailto:hello@joannahosking.com">hello@joannahosking.com &rarr;</a></p>
       <Slider shift={shift} setShift={setShift} />
       <div id="palettes-wrapper">
         <h2>Primary</h2>
@@ -22,7 +23,7 @@ function App() {
         <h2>Triadic</h2>
         <Triadic primary={primary} shift={shift} />
       </div>
-      <footer className="copyright"><p>Built by <a href="https://joannahosking.com/" target="_blank" rel="noreferrer">Joanna Hosking</a></p><p>&copy; {new Date().getFullYear()}</p></footer>
+      <footer className="copyright"><p><a href="https://buymeacoffee.com/joannahosking">Buy me a coffee?</a></p><p>&copy; {new Date().getFullYear()} <a href="https://joannahosking.com/" target="_blank" rel="noreferrer">Joanna Hosking</a></p></footer>
     </div>
   );
 }
